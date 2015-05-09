a671040fb2f59543bb1fe39ddfa0635673198858
Luciano Mammino <lmammino@oryzone.com>
2015-05-09 16:00:50 +0100

    decoupled url builder into separated internal module for better testing. Ref #3

--- test/Flickr.js
+++ test/Flickr.js
@@ -24,34 +24,4 @@ describe('Flickr', function() {
 
   });
 
-  describe('#Flickr.buildUrl', function(){
-
-    var apiKey = 'apiKey';
-    var f = new Flickr(apiKey);
-
-    it('should build an url for a method', function(){
-      var u = url.parse(f.buildUrl('someMethod', {foo:'bar', bar:'baz'}), true);
-      u.protocol.should.equal('https:');
-      u.hostname.should.equal('api.flickr.com');
-      u.pathname.should.equal('/services/rest');
-      u.query.api_key.should.equal(apiKey);
-      u.query.method.should.equal('someMethod');
-    });
-
-    it('should support an optional set of parameters', function(){
-      var u = url.parse(f.buildUrl('someMethod', {foo:'bar', bar:'baz'}), true);
-      u.query.foo.should.equal('bar');
-      u.query.bar.should.equal('baz');
-    });
-
-    it('should support an optional format', function(){
-      var u = url.parse(f.buildUrl('someMethod'), true);
-      u.query.format.should.be.a('string');
-
-      u = url.parse(f.buildUrl('someMethod', {}, 'someFormat'), true);
-      u.query.format.should.equal('someFormat');
-    });
-
-  });
-
 });
---
+++ test/apiUrlBuilder.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var buildUrl = require('../lib/apiUrlBuilder.js');
+var chai = require('chai');
+var url = require('url');
+
+chai.should();
+
+describe('apiUrlBuilder', function() {
+
+  describe('#apiUrlBuilder', function(){
+
+    var apiKey = 'apiKey';
+
+    it('should build an url for a method', function(){
+      var u = url.parse(buildUrl(apiKey, 'someMethod', {foo:'bar', bar:'baz'}), true);
+      u.protocol.should.equal('https:');
+      u.hostname.should.equal('api.flickr.com');
+      u.pathname.should.equal('/services/rest');
+      u.query.api_key.should.equal(apiKey);
+      u.query.method.should.equal('someMethod');
+    });
+
+    it('should support an optional set of parameters', function(){
+      var u = url.parse(buildUrl(apiKey, 'someMethod', {foo:'bar', bar:'baz'}), true);
+      u.query.foo.should.equal('bar');
+      u.query.bar.should.equal('baz');
+    });
+
+    it('should support an optional format', function(){
+      var u = url.parse(buildUrl(apiKey, 'someMethod'), true);
+      u.query.format.should.be.a('string');
+
+      u = url.parse(buildUrl(apiKey, 'someMethod', {}, 'someFormat'), true);
+      u.query.format.should.equal('someFormat');
+    });
+
+  });
+
+});
