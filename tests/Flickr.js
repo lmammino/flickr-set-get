db5457c80b92bc504aaef7ac73e377c946889904
Luciano Mammino <lmammino@oryzone.com>
2015-05-10 18:44:32 +0100

    Started async tests. Ref #3

--- tests/Flickr.js
+++ tests/Flickr.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var Flickr = require('../lib/Flickr.js');
 var chai = require('chai');
-var url = require('url');
+var nock = require('nock');
+var fs = require('fs');
+var Flickr = require('../lib/Flickr.js');
 
 chai.should();
 
@@ -26,9 +27,44 @@ describe('Flickr', function ƒ() {
 
   describe('#downloadSet()', function ƒ() {
 
+    it('should stop on invalid setId', function ƒ(done) {
+      var f = new Flickr('apiKey');
+
+      f.on('error', function onError(error) {
+        if (error.message.match(/Photoset not found/)) {
+          done();
+        }
+      });
+
+      nock('https://api.flickr.com')
+        .get('/services/rest?photoset_id=someInvalidSet&user_id=someUser&page=1' +
+          '&nojsoncallback=1&format=json&api_key=apiKey&method=flickr.photosets.getPhotos')
+        .reply(200, fs.readFileSync('./tests/fixtures/getPhotos_fail_setId.json', 'utf8'));
+
+      f.downloadSet('someInvalidSet', 'someUser');
+    });
+
+    it('should stop on invalid username', function ƒ(done) {
+      var f = new Flickr('apiKey');
+
+      f.on('error', function onError(error) {
+        if (error.message.match(/User not found/)) {
+          done();
+        }
+      });
+
+      nock('https://api.flickr.com')
+        .get('/services/rest?photoset_id=someSet&user_id=someInvalidUser&page=1' +
+        '&nojsoncallback=1&format=json&api_key=apiKey&method=flickr.photosets.getPhotos')
+        .reply(200, fs.readFileSync('./tests/fixtures/getPhotos_fail_userId.json', 'utf8'));
+
+      f.downloadSet('someSet', 'someInvalidUser');
+    });
+
     it('should download a set');
-    it('should stop on invalid setId');
-    it('should stop on invalid username');
+
+    it('should download a set with multiple pages');
+
     it('should skip not downloadable photos');
 
   });
